82a0e86bd5e6361bf034fb71fbb76642a1cbd723
Kristijan Kocev <kristijankocev1234@gmail.com>
2024-09-24 23:27:21 +0200

    feat: profile pic

--- src/app/components/sidebar/sections/UserInfo.tsx
+++ src/app/components/sidebar/sections/UserInfo.tsx
@@ -1,9 +1,18 @@
 import Engineer from '../../svg/Engineer'
 import Location from '../../svg/Location'
+import Image from 'next/image'
 
 const UserInfo = () => {
+  const isProd = process.env.NODE_ENV === 'production'
   return (
     <div className='flex flex-col items-center justify-center pt-8 pb-4'>
+      <Image
+        src={isProd ? '/portfolio/images/me.jpg' : '/images/me.jpg'}
+        className='rounded-[25px]'
+        width={100}
+        height={100}
+        alt='Rate Game Logo'
+      />
       <span>Kristijan Kocev</span>
       <div className='flex flex-row gap-4'>
         <div className='flex flex-row gap-1'>
