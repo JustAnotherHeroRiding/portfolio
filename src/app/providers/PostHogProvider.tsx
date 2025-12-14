7980d7196b1e54b249fc464856530502acfe6b0b
Kristijan Kocev <kristijan@mvpmasters.co>
2025-12-15 00:01:37 +0100

    chore: remove log

--- src/app/providers/PostHogProvider.tsx
+++ src/app/providers/PostHogProvider.tsx
@@ -10,8 +10,6 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   const posthogHost =
     process.env.NODE_ENV === 'production' ? process.env.POSTHOG_HOST : process.env.NEXT_PUBLIC_POSTHOG_HOST
 
-  console.log({ hasKey: Boolean(posthogKey), host: posthogHost })
-
   useEffect(() => {
     if (!posthogKey) return
 
