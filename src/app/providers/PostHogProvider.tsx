c8c73d4f0064c182864976e85d1e91b6e4b7ae5b
Kristijan Kocev <kristijan@mvpmasters.co>
2025-12-15 01:15:02 +0100

    fix: use env

--- src/app/providers/PostHogProvider.tsx
+++ src/app/providers/PostHogProvider.tsx
@@ -6,8 +6,10 @@ import { useEffect } from 'react'
 import SuspendedPostHogPageView from '../components/PostHogPageView'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  const posthogKey =
+    process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_POSTHOG_KEY : process.env.POSTHOG_KEY
   useEffect(() => {
-    posthog.init('phc_2ptcBqH8DDyTBtOBjxHNVnkI19I2VTaN2gtAYyg8Qf', {
+    posthog.init(posthogKey, {
       api_host: '/ingest',
       ui_host: 'https://us.posthog.com',
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
