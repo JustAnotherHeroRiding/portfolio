87653a272f28394b7418133c08cee20e951c213d
Kristijan Kocev <kristijan@mvpmasters.co>
2026-02-01 18:24:21 +0100

    fix: import path

--- src/app/components/sidebar/sections/UserInfo.tsx
+++ src/app/components/sidebar/sections/UserInfo.tsx
@@ -2,7 +2,7 @@ import Engineer from '../../svg/Engineer'
 import Location from '../../svg/Location'
 import Image from 'next/image'
 import Link from 'next/link'
-import meImage from '@/public/images/me.png'
+import meImage from '../../../../../public/images/me.png'
 
 const UserInfo = () => {
 
